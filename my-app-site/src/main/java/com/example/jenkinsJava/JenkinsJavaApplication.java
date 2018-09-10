d343ee79a22fb390a182cc0e184a76108067beaa
kevinfwp <kevinfwp@126.com>
2018-09-10 15:45:57 +0800

    springboot

---
+++ my-app-site/src/main/java/com/example/jenkinsJava/JenkinsJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJavaApplication.class, args);
+	}
+}
